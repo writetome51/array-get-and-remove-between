25d666667528b7100d192b85f904bdac256af9c1
Steve Thompson <writetome51@gmail.com>
2018-10-14 02:35:34 -0600

    changed function name to getAndRemoveBetween()

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 var errorIfNotInteger_1 = require("basic-data-handling/errorIfNotInteger");
 var getAndRemoveAdjacentAt_1 = require("@writetome51/array-get-and-remove-adjacent-at/getAndRemoveAdjacentAt");
 
-
-// Example: if array is [1,2,3,4,5,6,7] , and you call getAndRemoveMiddle(2, array)
+// Example: if array is [1,2,3,4,5,6,7] , and you call getAndRemoveBetween(2, array)
 // the array will be changed to [1,2,6,7] and the function will return [3,4,5] .  It
 // preserves the first 2 items and the last 2.
 
-function getAndRemoveMiddle(numItemsToKeepAtEachEnd, array) {
+function getAndRemoveBetween(numItemsToKeepAtEachEnd, array) {
 	errorIfNotInteger_1.errorIfNotInteger(numItemsToKeepAtEachEnd);
 	if (numItemsToKeepAtEachEnd < 1)
 		throw new Error('The number of items to' +
@@ -22,4 +21,5 @@ function getAndRemoveMiddle(numItemsToKeepAtEachEnd, array) {
 	return getAndRemoveAdjacentAt_1.getAndRemoveAdjacentAt(numItemsToKeepAtEachEnd, numToRemove, array);
 }
 
-exports.getAndRemoveMiddle = getAndRemoveMiddle;
+
+exports.getAndRemoveBetween = getAndRemoveBetween;
--- lib/getAndRemoveBetween.ts
+++ lib/getAndRemoveBetween.ts
@@ -3,11 +3,11 @@ import { getAndRemoveAdjacentAt }
 	from '@writetome51/array-get-and-remove-adjacent-at/getAndRemoveAdjacentAt';
 
 
-// Example: if array is [1,2,3,4,5,6,7] , and you call getAndRemoveMiddle(2, array)
+// Example: if array is [1,2,3,4,5,6,7] , and you call getAndRemoveBetween(2, array)
 // the array will be changed to [1,2,6,7] and the function will return [3,4,5] .  It
 // preserves the first 2 items and the last 2.
 
-export function getAndRemoveMiddle(numItemsToKeepAtEachEnd, array): any[] {
+export function getAndRemoveBetween(numItemsToKeepAtEachEnd, array): any[] {
 	errorIfNotInteger(numItemsToKeepAtEachEnd);
 	if (numItemsToKeepAtEachEnd < 1) throw new Error('The number of items to' +
 		' preserve at each end must be at least 1');
