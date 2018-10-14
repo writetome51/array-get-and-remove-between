6f6bdde7d5ec3c68e1abdda85d86ce2ebb288d41
Steve Thompson <writetome51@gmail.com>
2018-10-13 21:58:29 -0600

    added tests

--- lib/getAndRemoveMiddle.ts
+++ lib/getAndRemoveMiddle.ts
@@ -11,9 +11,10 @@ export function getAndRemoveMiddle(numItemsToKeepAtEachEnd, array): any[] {
 	errorIfNotInteger(numItemsToKeepAtEachEnd);
 	if (numItemsToKeepAtEachEnd < 1) throw new Error('The number of items to' +
 		' preserve at each end must be at least 1');
-	if ((numItemsToKeepAtEachEnd * 2) > array.length) {
+	if ((numItemsToKeepAtEachEnd * 2) >= array.length) {
 		throw new Error('The number of items to preserve multiplied by 2' +
-			' exceeds the length of the array. This is not allowed.');
+			' is either equal to or exceeds the length of the array. This is not' +
+			' allowed.');
 	}
 	let numToRemove = array.length - (numItemsToKeepAtEachEnd * 2);
 	return getAndRemoveAdjacentAt(numItemsToKeepAtEachEnd, numToRemove, array);
---
+++ tests.js
@@ -0,0 +1,78 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var getAndRemoveMiddle_1 = require("./lib/getAndRemoveMiddle");
+var arraysMatch_1 = require("@writetome51/arrays-match/arraysMatch");
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Test 1:
+var middle = getAndRemoveMiddle_1.getAndRemoveMiddle(1, arr);
+if (arraysMatch_1.arraysMatch(middle, [2, 3, 4, 5, 6, 7, 8, 9]))
+    console.log('test 1 passed');
+else
+    console.log('test 1 failed');
+// Test 2: make sure those items were removed from array:
+if (arraysMatch_1.arraysMatch(arr, [1, 10]))
+    console.log('test 2 passed');
+else
+    console.log('test 2 failed');
+// Test 3:
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+middle = getAndRemoveMiddle_1.getAndRemoveMiddle(2, arr);
+if (arraysMatch_1.arraysMatch(middle, [3, 4, 5, 6, 7, 8]))
+    console.log('test 3 passed');
+else
+    console.log('test 3 failed');
+// Test 4: make sure those items were removed from array:
+if (arraysMatch_1.arraysMatch(arr, [1, 2, 9, 10]))
+    console.log('test 4 passed');
+else
+    console.log('test 4 failed');
+// Test 5:
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+middle = getAndRemoveMiddle_1.getAndRemoveMiddle(4, arr);
+if (arraysMatch_1.arraysMatch(middle, [5, 6, 7, 8, 9, 10, 11, 12]))
+    console.log('test 5 passed');
+else
+    console.log('test 5 failed');
+// Test 6: make sure those items were removed from array:
+if (arraysMatch_1.arraysMatch(arr, [1, 2, 3, 4, 13, 14, 15, 16]))
+    console.log('test 6 passed');
+else
+    console.log('test 6 failed');
+// Test 7:
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+middle = getAndRemoveMiddle_1.getAndRemoveMiddle(6, arr);
+if (arraysMatch_1.arraysMatch(middle, [7, 8, 9, 10]))
+    console.log('test 7 passed');
+else
+    console.log('test 7 failed');
+// Test 8: make sure those items were removed from array:
+if (arraysMatch_1.arraysMatch(arr, [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16]))
+    console.log('test 8 passed');
+else
+    console.log('test 8 failed');
+// Test 9:
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+middle = getAndRemoveMiddle_1.getAndRemoveMiddle(8, arr);
+if (arraysMatch_1.arraysMatch(middle, [9]))
+    console.log('test 9 passed');
+else
+    console.log('test 9 failed');
+// Test 10: make sure those items were removed from array:
+if (arraysMatch_1.arraysMatch(arr, [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17]))
+    console.log('test 10 passed');
+else
+    console.log('test 10 failed');
+// Test 11: if there is no middle past the number of items you want to ignore, trigger
+// error:
+var errorTriggered = false;
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+try {
+    middle = getAndRemoveMiddle_1.getAndRemoveMiddle(8, arr);
+}
+catch (e) {
+    errorTriggered = true;
+}
+if (errorTriggered)
+    console.log('test 11 passed');
+else
+    console.log('test 11 failed');
